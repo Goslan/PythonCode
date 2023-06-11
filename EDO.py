356320863b6cbe7d7c4bf3536776b8787107d881
Goslan <88907050+Goslan@users.noreply.github.com>
2023-06-11 19:45:35 -0300

    Add files via upload

---
+++ EDO.py
@@ -0,0 +1,32 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Parâmetros
+num_points = 100  # Número de pontos no domínio
+x_start = 0.0  # Início do domínio
+x_end = 1.0  # Fim do domínio
+x = np.linspace(x_start, x_end, num_points)  # Discretização do domínio
+
+# Condições iniciais
+y = np.zeros(num_points)  # Valores iniciais de y
+y_prime = np.zeros(num_points)  # Valores iniciais de y'
+
+# Iterações
+num_iterations = 1000  # Número de iterações
+delta_t = 0.001  # Tamanho do passo de tempo
+
+for i in range(num_iterations):
+    y_double_prime = np.gradient(y_prime, x)  # Cálculo da segunda derivada de y
+    n = 1 / y  # Cálculo de n usando a condição dada
+
+    # Atualização dos valores de y e y'
+    y = y + delta_t * y_prime
+    y_prime = y_prime + delta_t * (y_double_prime * n * np.sqrt(1 + y_prime**2) - y_double_prime * y_prime**2 / (n * (1 + y_prime**2)**(3/2)) - y_prime * np.gradient(n, x) / (n**2 * np.sqrt(1 + y_prime**2)) - np.sqrt(1 + y_prime**2) * np.gradient(n, y) / (n**2))
+
+# Plot da solução
+plt.plot(x, y)
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('Solução da EDP')
+plt.grid(True)
+plt.show()
